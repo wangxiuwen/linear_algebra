2c13545b418ae9fd72bae24e77ac68df7bb20ed8
wangxiuwen <wangxiuwen@qianrushi.org>
2017-08-02 17:31:00 +0800

    fix

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class Vector(object):
     NO_UNIQUE_PARALLEL_COMPONENT_MSG = 'No unique parallel component'
     ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG = 'Cross function is only defined for 2d and 3d'
 
-    
     def __init__(self, coordinates):
         try:
             if not coordinates:
@@ -27,43 +26,17 @@ class Vector(object):
 
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
-
-    def __iter__(self):
-        self.current = 0
-        return self
-
-    def next(self):
-        if self.current >= len(self.coordinates):
-            raise StopIteration
-        else:
-            current_value = self.coordinates[self.current]
-            self.current += 1
-            return current_value
-
-    def __len__(self):
-        return len(self.coordinates)
-
-    def __getitem__(self, i):
-        return self.coordinates[i]
-
+    
+    # 打印向量
     def __str__(self):
         # return 'Vector: {}'.format(self.coordinates)
-        return 'Vector: {}'.format([round(coord, 3)
-                                    for coord in self.coordinates])
-
-    def __eq__(self, v):
-        return self.coordinates == v.coordinates
-    
-    '''
-    def is_zero(self):
-        return set(self.coordinates) == set([Decimal(0)])
-    def is_near_zero(self, eps=1e-10):
-        return abs(self) < eps
-    '''
-    
+        return 'Vector: {}'.format([round(coord, 3) for coord in self.coordinates])
+        
+    # 判断是否为0向量
     def is_zero(self, tolerance=1e-10):
         return self.magnitude() < tolerance
     
+    # 计算长度
     def magnitude(self):
         # return sqrt(sum([x**2 for x in self.coordinates]))
         return Decimal(sqrt(sum([coord * coord
@@ -73,16 +46,15 @@ class Vector(object):
         # return Vector([x+y for x,y in zip(self.coordinates, v.coordinates)])
         return Vector(map(sum, zip(self.coordinates, v.coordinates)))
 
-    def minus(self, other):
-        # return Vector([x-y for x,y in zip(self.coordinates, v.coordinates)])
-        return Vector([coords[0] - coords[1]
-                       for coords in zip(self.coordinates, other.coordinates)])
+    def minus(self, v):
+        return Vector([x-y for x,y in zip(self.coordinates, v.coordinates)])
+        #return Vector([coords[0] - coords[1] for coords in zip(self.coordinates, v.coordinates)])
 
+    # 乘以标量
     def times_scalar(self, factor):
         return Vector([Decimal(factor) * coord for coord in self.coordinates])
    
-
-
+    # 标准化
     def normalized(self):
         try:
             # return self.times_scalar(1./self.magnitude())
@@ -90,44 +62,11 @@ class Vector(object):
         except ZeroDivisionError:
             raise Exception(CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
 
+    # 点积
     def dot(self, v):
         return sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
 
-    def get_angle_rad(self, other):
-        dot_prod = round(self.normalized().dot(other.normalized()), 3)
-        return acos(dot_prod)
-
-    def get_angle_deg(self, other):
-        degrees_per_rad = 180. / pi
-        return degrees_per_rad * self.get_angle_rad(other)
-
-    def is_parallel(self, other):
-        return (self.is_zero() or other.is_zero() or
-                self.get_angle_rad(other) in [0, pi])
-
-    def is_orthogonal(self, other):
-        return round(self.dot(other), 3) == 0
-
-    def get_projected_vector(self, other):
-        """
-        Gets projection of vector v in b
-        """
-        b_normalized = other.normalized()
-        return b_normalized.times_scalar(self.dot(b_normalized))
-
-    def get_orthogonal_vector(self, other):
-        return self.minus(self.get_projected_vector(other))
-
-    def component_orthogonal_to(self, basis):
-        try:
-            projection = self.component_parallel_to(basis)
-            return self.minus(projection)
-        except Exception as e:
-            if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
-                raise Exception(self.NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG)
-            else:
-                raise e
-    
+    # 向量投影 
     def component_parallel_to(self, basis):
         try:
             u = basis.normalized()
@@ -139,12 +78,27 @@ class Vector(object):
             else:
                 raise e
     
+    # 正交分量
+    def component_orthogonal_to(self, basis):
+        try:
+            projection = self.component_parallel_to(basis)
+            return self.minus(projection)
+        except Exception as e:
+            if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+                raise Exception(self.NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG)
+            else:
+                raise e
+    
+    # 判断正交
     def is_orthogonal_to(self, v, tolerance=1e-10):
+        # return round(self.dot(other), 3) == 0
         return abs(self.dot(v)) < tolerance
-        
+    
+    # 判断平行  
     def is_parallel_to(self, v):
         return abs(self.is_zero() or v.is_zero() or self.angle_with(v) == 0 or self.angle_with(v) == pi)
-            
+    
+    # 计算向量夹角
     def angle_with(self, v, in_degrees=False):
         try:
             n1 = self.normalized()
@@ -164,11 +118,12 @@ class Vector(object):
                 raise Exception('Cannot compute an angle with the zero vector')
             else:
                 raise e
+    
+    # 计算叉积
     def cross(self, v):
         try:
             x_1, y_1, z_1 = self.coordinates
             x_2, y_2, z_2 = v.coordinates
-
             return Vector([y_1 * z_2 - y_2 * z_1, -(x_1 * z_2 - x_2 * z_1), x_1 * y_2 - x_2 * y_1])
         except ValueError as e:
             msg = str(e)
@@ -180,158 +135,122 @@ class Vector(object):
                 raise Exception(self.ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG)
             else:
                 raise e
-
+    
+    # 计算平行四边形面积
+    def area_of_parallelogram_with(self, v):
+        return self.cross(v).magnitude()
+    
+    #计算三角形面积
     def area_of_triangle_with(self, v):
         return self.area_of_parallelogram_with(v) / Decimal('2.0')
 
-    def area_of_parallelogram_with(self, v):
-        return self.cross(v).magnitude()                
-    '''
-    def cross_product(self, other):
-        [x1, y1, z1] = self.coordinates
-        [x2, y2, z2] = other.coordinates
-        x = (y1 * z2) - (y2 * z1)
-        y = -((x1 * z2) - (x2 * z1))
-        z = (x1 * y2) - (x2 * y1)
-        return Vector([x, y, z])
-
-    def area_parallelogram(self, other):
-        return self.cross_product(other).magnitude()
-
-    def area_triangle(self, other):
-        return self.cross_product(other).magnitude() / 2
-    '''    
-    
 if __name__ == '__main__':
 
     # 判断两个向量是否相等
-    my_vector = Vector([1,2,3])
-    my_vector2 = Vector([1,2,3])
-    print my_vector	== my_vector2
+    v = Vector([1,2,3])
+    w = Vector([1,2,3])
+    print v	== w
 
+    # 向量相加
     v = Vector([8.218, -9.341])
     w = Vector([-1.129, 2.111])
     addition = v.plus(w)
     print 'addition: {}'.format(addition)
     
+    # 向量相减
     v = Vector([7.119, 8.215])
     w = Vector([-8.223, 0.878])
     subtraction = v.minus(w)
     print 'subtraction: {}'.format(subtraction)
 
+    # 与标量相乘
     v = Vector([1.671, -1.012, -0.318])
     multiplication = v.times_scalar(7.41)
     print 'multiplication: {}'.format(multiplication)
 
-    # *****************
-
+    # 计算长度
     v = Vector([-0.221, 7.437])
+    w = Vector([8.813, -1.331, -6.247])
+    
     first_magintude = v.magnitude()
     print 'first_magintude: {}'.format(round(first_magintude, 3))
 
-    v = Vector([8.813, -1.331, -6.247])
-    second_magintude = v.magnitude()
+    second_magintude = w.magnitude()
     print 'second_magintude: {}'.format(round(second_magintude, 3))
 
+    # 向量标准化
     v = Vector([5.581, -2.136])
+    w = Vector([1.996, 3.108, -4.554])
+    
     first_normalization = v.normalized()
     print 'first_normailization: {}'.format(first_normalization)
 
-    v = Vector([1.996, 3.108, -4.554])
-    second_normalization = v.normalized()
+    second_normalization = w.normalized()
     print 'second_normailization: {}'.format(second_normalization)
 
-    # *****************
     # 求 v, w 点积
-    v = Vector([7.887, 4.138])
-    w = Vector([-8.802, 6.776])
-    dot = v.dot(w)
+    v1 = Vector([7.887, 4.138])
+    w1 = Vector([-8.802, 6.776])
+    
+    v2 = Vector([-5.955, -4.904, -1.874])
+    w2 = Vector([-4.496, -8.755, 7.103])
+    
+    dot = v1.dot(w1)
     print 'first_dot: {}'.format(round(dot, 3))
 
-    # 求 v, w 点积
-    v = Vector([-5.955, -4.904, -1.874])
-    w = Vector([-4.496, -8.755, 7.103])
-    dot = v.dot(w)
+    dot = v2.dot(w2)
     print 'second_dot: {}'.format(round(dot, 3))
 
-    # *****************
     # 求 v, w 夹角, 单位rad
     v = Vector([3.183, -7.627])
     w = Vector([-2.668, 5.319])
-    angle_rads = v.get_angle_rad(w)
+    angle_rads = v.angle_with(w)
     print 'first_angle_rads: {}'.format(angle_rads)
 
     # 求 v, w 夹角, 单位度
     v = Vector([7.35, 0.221, 5.188])
     w = Vector([2.751, 8.259, 3.985])
-    angle_degrees = v.get_angle_deg(w)
+    angle_degrees = v.angle_with(w, True)
     print 'first_angle_rads: {}'.format(angle_degrees)
 
-    # *****************
     # 判断向量平行还是正交
-    v = Vector([-7.579, -7.88])
-    w = Vector([22.737, 23.64])
-    is_parallel = v.is_parallel(w)
-    is_orthogonal = v.is_orthogonal(w)
-
+    v1 = Vector([-7.579, -7.88])
+    w1 = Vector([22.737, 23.64])
+    
+    v2 = Vector([-2.029, 9.97, 4.172])
+    w2 = Vector([-9.231, -6.639, -7.245])
+    
+    v3 = Vector([-2.328, -7.284, -1.214])
+    w3 = Vector([-1.821, 1.072, -2.94])
+    
+    v4 = Vector([2.118, 4.827])
+    w4 = Vector([0, 0])
+    
+    is_parallel = v1.is_parallel_to(w1)
+    is_orthogonal = v1.is_orthogonal_to(w1)
     print '1 parallel: {}, orthogonal: {}'.format(is_parallel, is_orthogonal)
 
-    v = Vector([-2.029, 9.97, 4.172])
-    w = Vector([-9.231, -6.639, -7.245])
-    is_parallel = v.is_parallel(w)
-    is_orthogonal = v.is_orthogonal(w)
-
+    is_parallel = v2.is_parallel_to(w2)
+    is_orthogonal = v2.is_orthogonal_to(w2)
     print '2 parallel: {}, orthogonal: {}'.format(is_parallel, is_orthogonal)
 
-    v = Vector([-2.328, -7.284, -1.214])
-    w = Vector([-1.821, 1.072, -2.94])
-    is_parallel = v.is_parallel(w)
-    is_orthogonal = v.is_orthogonal(w)
+    is_parallel = v3.is_parallel_to(w3)
+    is_orthogonal = v3.is_orthogonal_to(w3)
     print '3 parallel: {}, orthogonal: {}'.format(is_parallel, is_orthogonal)
 
-    v = Vector([2.118, 4.827])
-    w = Vector([0, 0])
-    is_parallel = v.is_parallel(w)
-    is_orthogonal = v.is_orthogonal(w)
-
+    is_parallel = v4.is_parallel_to(w4)
+    is_orthogonal = v4.is_orthogonal_to(w4)
     print '4 parallel: {}, orthogonal: {}'.format(is_parallel, is_orthogonal)
 
-    # *****************
-
+    # 计算投影
     v = Vector([3.039, 1.879])
     w = Vector([0.825, 2.036])
-    projected_vector = v.get_projected_vector(w)
-
-    print 'projected vector is: {}'.format(projected_vector)
+    print 'component_parallel_to: {}'.format(v.component_parallel_to(w))
 
     v = Vector([-9.88, -3.264, -8.159])
     w = Vector([-2.155, -9.353, -9.473])
-    orthogonal_vector = v.get_orthogonal_vector(w)
-
-    print 'orthogonal vector is: {}'.format(orthogonal_vector)
-
-    v = Vector([3.009, -6.172, 3.692, -2.51])
-    w = Vector([6.404, -9.144, 2.759, 8.718])
-    projected_vector = v.get_projected_vector(w)
-    orthogonal_vector = v.get_orthogonal_vector(w)
-
-    print 'second projected vector is: {}'.format(projected_vector)
-
-    print 'second orthogonal vector is: {}'.format(orthogonal_vector)
-
-
-    # -----------------------------------------------------
-    print '#1'
-    v = Vector([3.039, 1.879])
-    w = Vector([0.825, 2.036])
-    print v.component_parallel_to(w)
-
-    print '\n#2'
-    v = Vector([-9.88, -3.264, -8.159])
-    w = Vector([-2.155, -9.353, -9.473])
-    print v.component_orthogonal_to(w)
-
-    print '\n#3'
+    print 'component_orthogonal_to: {}'.format(v.component_orthogonal_to(w))
+ 
     v = Vector([3.009, -6.172, 3.692, -2.51])
     w = Vector([6.404, -9.144, 2.759, 8.718])
     vpar = v.component_parallel_to(w)
@@ -339,7 +258,7 @@ if __name__ == '__main__':
     print 'parallel component:', vpar
     print 'orthogonal component:', vort
     
-    # -----------------------------------------------------
+    # 计算叉积
     v1 = Vector([8.462, 7.893, -8.187])
     w1 = Vector([6.984, -5.975, 4.778])
 
